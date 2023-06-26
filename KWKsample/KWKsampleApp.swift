d1de657e744658b88526ba551a01891f18da845c
Aditi Honna <aditihonna@gmail.com>
2023-06-26 15:24:19 -0400

    Initial Commit

---
+++ KWKsample/KWKsampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKsampleApp.swift
+//  KWKsample
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct KWKsampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
